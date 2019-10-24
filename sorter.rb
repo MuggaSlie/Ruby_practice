9e1ae8300c99282e0ed5f37637c5cc0b20560cfe
Justin Holmes <mogglethesly@gmail.com>
2019-10-23 20:10:18 -0700

    Updates

--- sorter.rb
+++ sorter.rb
@@ -1,17 +1,22 @@
 #sorting program
 
 puts 'Hello, This program will sort as many words as you like. Each word must be entered separately. If you hit enter (return) on an empty line sorter will return results, then program will exit. '
-words = []
-word =''
-  while true
-    puts 'Please enter a single word:'
-    word = gets.chomp
-    if word != ''
-      words.push word
-    else
-      words = words.sort
-      break
-    end
-  end
 
-puts words.join (', ')
+def sorter
+  words = []
+  word =''
+  while true
+      puts 'Please enter a single word:'
+      word = gets.chomp
+      if word != ''
+        words.push word
+      else
+        words = words.sort
+        break
+      end
+  end
+  puts 'The words ordered are:'
+  puts words.join (', ')
+end
+
+sorter
